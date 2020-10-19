cb76549e4d7db1a1d2fde32ce8a40a3b6a04c950
lamossus <zhora.polyakov95@mail.ru>
2020-10-19 13:32:50 +0300

    Initial commit

---
+++ src/laba8/UnfairWaitList.java
@@ -0,0 +1,36 @@
+package laba8;
+import java.util.Collection;
+
+public class UnfairWaitList<E> extends WaitList<E> {
+
+    UnfairWaitList() {
+
+    }
+
+    UnfairWaitList(Collection<E> c) {
+        content.addAll(c);
+    }
+
+    public void remove(E element) {
+        if (content.element() == element) {
+            content.remove(element);
+        }
+    }
+
+    public void add(E element) {
+        content.add(element);
+    }
+
+    public void moveToBack(E element) {
+        if (contains(element)) {
+            remove(element);
+            content.add(element);
+        }
+    }
+
+    public String toString() {
+        return "UnfairWaitList{" +
+                "content=" + content +
+                '}';
+    }
+}
